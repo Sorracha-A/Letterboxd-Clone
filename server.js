d8733846aa86b693ab75a3c8addef07e68424a2e
Sorracha-A <90688030+Sorracha-A@users.noreply.github.com>
2023-02-11 17:37:00 +0700

    connect each routes
    connect ever pages on the navbar

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const filmsRouter = require('./routes/films');
 const registerRouter = require('./routes/create-account');
 const listRouter = require('./routes/lists');
+const membersRouter = require('./routes/members');
+const journalRouter = require('./routes/journal');
+const signinRouter = require('./routes/sign-in');
 
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
 // app.use("/route", express.static("foldername"));
@@ -11,6 +14,9 @@ app.set('view engine','ejs')
 app.use('/films',filmsRouter);
 app.use('/lists',listRouter);
 app.use('/create-account',registerRouter);
+app.use('/members',membersRouter);
+app.use('/journal',journalRouter);
+app.use('/sign-in',signinRouter);
 app.get('/', (req, res) => {
   res.render('homepage/index');
 });
