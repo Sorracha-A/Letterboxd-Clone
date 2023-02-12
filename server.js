727f7d1622863c178c5fe27418465f8129fbb915
Sorracha-A <64011628@kmitl.ac.th>
2023-02-13 03:47:36 +0700

    Implemented API for Popular, Now Playing, and Upcoming Films with Randomized Order for Now Playing.
    -Implemented APIs for displaying popular, now playing, and upcoming films.
    -Randomized the order of now playing films every time the website is entered.
    -Made every film on the index page clickable.
    -Created a route and template to handle all films in the TMDB, allowing access to any movie via URL.
    -Search functionality is still not working.

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const axios = require("axios");
-const filmsRouter = require('./routes/navbar/films');
-const registerRouter = require('./routes/navbar/create-account');
-const listRouter = require('./routes/navbar/lists');
-const membersRouter = require('./routes/navbar/members');
-const journalRouter = require('./routes/navbar/journal');
-const signinRouter = require('./routes/navbar/sign-in');
+const filmsRouter = require("./routes/navbar/films");
+const registerRouter = require("./routes/navbar/create-account");
+const listRouter = require("./routes/navbar/lists");
+const membersRouter = require("./routes/navbar/members");
+const journalRouter = require("./routes/navbar/journal");
+const signinRouter = require("./routes/navbar/sign-in");
 
-app.use('/public', express.static('public'));
-app.set('view engine','ejs')
-app.use('/films',filmsRouter);
-app.use('/lists',listRouter);
-app.use('/create-account',registerRouter);
-app.use('/members',membersRouter);
-app.use('/journal',journalRouter);
-app.use('/sign-in',signinRouter);
+app.use("/public", express.static("public"));
+app.set("view engine", "ejs");
+app.use("/films", filmsRouter);
+app.use("/lists", listRouter);
+app.use("/create-account", registerRouter);
+app.use("/members", membersRouter);
+app.use("/journal", journalRouter);
+app.use("/sign-in", signinRouter);
 
 app.get("/", async (req, res) => {
   try {
@@ -23,8 +23,21 @@ app.get("/", async (req, res) => {
       "https://api.themoviedb.org/3/movie/popular?api_key=5042d23af1cc65852a1dea00714c63fd"
     );
     const movieData = response.data;
-    console.log(movieData);
-    res.render("homepage/index", { movieData });
+
+    const nowPlayingResponse = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=5042d23af1cc65852a1dea00714c63fd&language=en-US"
+    );
+    const nowPlayingData = nowPlayingResponse.data;
+
+    const upcomingResponse = await axios.get(
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=5042d23af1cc65852a1dea00714c63fd&language=en-US"
+    );
+    const upcomingData = upcomingResponse.data;
+
+    console.log(nowPlayingData);
+    res.render("homepage/index", {
+      movieData, nowPlayingData,upcomingData
+    });
   } catch (err) {
     console.error(err);
     res.render("homepage/index");
@@ -33,17 +46,28 @@ app.get("/", async (req, res) => {
 
 app.get("/film/:title", async (req, res) => {
   try {
-    const movieTitle = req.params.title.replace(/-/g, ' ');
-    const response = await axios.get(
+    const movieTitle = req.params.title.replace(/-/g, " ");
+    const searchResponse = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=5042d23af1cc65852a1dea00714c63fd&query=${movieTitle}`
     );
-    const movieData = response.data;
-    console.log(movieData);
-    res.render("film/film", { movieData });
+    const movieId = searchResponse.data.results[0].id;
+    const movieResponse = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=5042d23af1cc65852a1dea00714c63fd`
+    );
+    const movieData = movieResponse.data;
+    const movieCredits = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=5042d23af1cc65852a1dea00714c63fd`
+    );
+    const directors = movieCredits.data.crew.filter(
+      (member) => member.job === "Director"
+    );
+    const directorNames = directors.map((director) => director.name).join(", ");
+
+    res.render("film/film", { movieData, directorNames });
   } catch (err) {
     console.error(err);
     res.render("homepage/film");
   }
 });
 
-app.listen(3500)
+app.listen(3500);
