5c967b03698951c6fdcf33d591d71c07e7210867
Sorracha-A <64011628@kmitl.ac.th>
2023-02-12 19:36:25 +0700

    (testing) logs out the popular movies api. also, the data will replace in the ejs file after editing the ejs files.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require("axios");
 const filmsRouter = require('./routes/navbar/films');
 const registerRouter = require('./routes/navbar/create-account');
 const listRouter = require('./routes/navbar/lists');
@@ -7,8 +8,6 @@ const membersRouter = require('./routes/navbar/members');
 const journalRouter = require('./routes/navbar/journal');
 const signinRouter = require('./routes/navbar/sign-in');
 
-// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
-// app.use("/route", express.static("foldername"));
 app.use('/public', express.static('public'));
 app.set('view engine','ejs')
 app.use('/films',filmsRouter);
@@ -17,8 +16,19 @@ app.use('/create-account',registerRouter);
 app.use('/members',membersRouter);
 app.use('/journal',journalRouter);
 app.use('/sign-in',signinRouter);
-app.get('/', (req, res) => {
-  res.render('homepage/index');
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=5042d23af1cc65852a1dea00714c63fd"
+    );
+    const movieData = response.data;
+    console.log(movieData);
+    res.render("homepage/index", { movieData });
+  } catch (err) {
+    console.error(err);
+    res.render("homepage/index");
+  }
 });
 
 app.listen(3500)
